f0ce5ebcd88c32d6be1a8738fed05de46c71917c
JustinM <justin.moehringer@Justins-iMac.fios-router.home>
2017-05-23 04:20:51 -0400

    changed vaporProvider to work without NMSSH

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VaporFlock",
     dependencies: [
-        .Package(url: "https://github.com/JustinM1/Flock.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/JustinM1/Flock.git", Version(0,1,5)),
     ]
 )
