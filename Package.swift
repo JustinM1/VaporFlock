04bc76c55c2972b0c67b006ba135cdd0f8d11e9f
JustinM <justin.moehringer@Justins-iMac.fios-router.home>
2017-05-23 09:27:38 -0400

    updated provider to recent syntax

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VaporFlock",
     dependencies: [
-        .Package(url: "https://github.com/JustinM1/Flock.git", Version(0,1,5)),
+        .Package(url: "https://github.com/JustinM1/Flock.git", Version(0,2,0)),
     ]
 )
--- Sources/VaporFlock.swift
+++ Sources/VaporFlock.swift
@@ -1,73 +1,19 @@
 
+
 import Flock
-import Foundation
 
 public extension Flock {
-  static let Vapor: [Task] = [
-    StopTask(),
-    StartTask(),
-    PsTask()
-  ]
+  static let Vapor: [Task] = SupervisordTask(provider: VaporSupervisord()).provider.createTasks()
 }
 
-public extension Config {
-  static var outputLog = "/dev/null"
-  static var errorLog = "/dev/null"
-}
-
-let vapor = "vapor"
-
-public class StopTask: Task {
-  public let name = "stop"
-  public let namespace = vapor
-  public let hookTimes: [HookTime] = [.before("deploy:link")]
+class VaporSupervisord: SupervisordProvider {
+  let taskNamespace = "vapor"
+  let supervisordName = Config.supervisordName ?? "vapor"
   
-  public func run(on server: Server) throws {
-    if let pid = try findServerPid(on: server) {
-      try server.execute("kill -9 \(pid)")
-    } else {
-      print("Vapor not running")
-    }
-  }
-}
-
-public class StartTask: Task {
-  public let name = "start"
-  public let namespace = vapor
-  public let hookTimes: [HookTime] = [.after("deploy:link")]
-  
-  public func run(on server: Server) throws {
-    print("Starting Vapor")
-    try server.execute("nohup \(Paths.executable) >> \(Config.outputLog) 2>> \(Config.errorLog) &")
-    try invoke("vapor:ps")
-  }
-}
-
-public class PsTask: Task {
-  public let name = "ps"
-  public let namespace = vapor
-  
-  public func run(on server: Server) throws {
-    if let pid = try findServerPid(on: server) {
-      print("Vapor running as process \(pid)")
-    } else {
-      print("Vapor not running")
-    }
-  }
-}
-
-private func findServerPid(on server: Server) throws -> String? {
-  guard let processes = try server.capture("ps aux | grep \".build\"") else {
-    return nil
+  func confFile(for server: Server) -> SupervisordConfFile {
+    var file = SupervisordConfFile(programName: supervisordName)
+    file.command += " --env=\(Config.environment)"
+    return file
   }
   
-  let lines = processes.components(separatedBy: "\n")
-  for line in lines where !line.contains("grep") {
-    let segments = line.components(separatedBy: " ").filter { !$0.isEmpty }
-    if segments.count > 1 {
-      return segments[1]
-    }
-    return segments.count > 1 ? segments[1] : nil
-  }
-  return nil
 }
