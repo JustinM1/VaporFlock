878e460041cd3c09d2e96d84f197dcf163a74f0f
JustinM1 <JustinM1@users.noreply.github.com>
2017-05-25 02:24:00 -0400

    Update VaporFlock.swift

--- Sources/VaporFlock.swift
+++ Sources/VaporFlock.swift
@@ -1,19 +1,17 @@
-
-
 import Flock
 
 public extension Flock {
-  static let Vapor: [Task] = SupervisordTask(provider: VaporSupervisord()).provider.createTasks()
+    static let Vapor: [Task] = SupervisordTasks(provider: VaporSupervisord()).createTasks()
 }
 
 class VaporSupervisord: SupervisordProvider {
-  let taskNamespace = "vapor"
-  let supervisordName = Config.supervisordName ?? "vapor"
-  
-  func confFile(for server: Server) -> SupervisordConfFile {
-    var file = SupervisordConfFile(programName: supervisordName)
-    file.command += " --env=\(Config.environment)"
-    return file
-  }
-  
+    let taskNamespace = "vapor"
+    let supervisordName = Config.supervisordName ?? "vapor"
+    
+    func confFile(for server: Server) -> SupervisordConfFile {
+        var file = SupervisordConfFile(programName: supervisorCtlName)
+        file.command += " --env=\(Config.environment)"
+        return file
+    }
+    
 }
