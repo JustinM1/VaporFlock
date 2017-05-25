de28e98bd11a668092273269176f0c135d8c133d
JustinM <justin.moehringer@Justins-iMac.fios-router.home>
2017-05-25 02:50:26 -0400

    corrected build errors

--- Sources/VaporFlock.swift
+++ Sources/VaporFlock.swift
@@ -1,17 +1,17 @@
 import Flock
 
 public extension Flock {
-    static let Vapor: [Task] = SupervisordTasks(provider: VaporSupervisord()).createTasks()
+  static let Vapor: [Task] = VaporSupervisord.init().createTasks()
 }
 
 class VaporSupervisord: SupervisordProvider {
-    let taskNamespace = "vapor"
-    let supervisordName = Config.supervisordName ?? "vapor"
-    
-    func confFile(for server: Server) -> SupervisordConfFile {
-        var file = SupervisordConfFile(programName: supervisorCtlName)
-        file.command += " --env=\(Config.environment)"
-        return file
-    }
-    
+  let taskNamespace = "vapor"
+  let supervisordName = Config.supervisordName ?? "vapor"
+  
+  func confFile(for server: Server) -> SupervisordConfFile {
+    var file = SupervisordConfFile(programName: supervisordName)
+    file.command += " --env=\(Config.environment)"
+    return file
+  }
+  
 }
